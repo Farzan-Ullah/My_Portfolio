831234233a96676d34e6d9f067cabb1b60ce653b
farzan <farzanullah07@gmail.com>
2024-05-14 23:38:54 +0530

    ready to deploy

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/emailIcon.png")}
+            alt="Email icon"
+            onClick={() =>
+              window.open("mailto:farzanullah07@gmail.com", "_blank")
+            }
+            style={{ cursor: "pointer" }}
+          />
+          <a href="mailto:farzanullah07@gmail.com">farzanullah07@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+            onClick={() =>
+              window.open("https://www.linkedin.com/in/farzanofme/", "_blank")
+            }
+            style={{ cursor: "pointer" }}
+          />
+          <a href="https://www.linkedin.com/in/farzanofme/">linkedin/Farzan</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/githubIcon.png")}
+            alt="Github icon"
+            onClick={() =>
+              window.open("https://github.com/Farzan-Ullah", "_blank")
+            }
+            style={{ cursor: "pointer" }}
+          />
+          <a href="https://github.com/Farzan-Ullah">github/Farzan</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Farzan Ullah</h1>
+        <p className={styles.description}>
+          {/* I'm a Software Developer with 3 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more! */}
+          Software Engineer dedicated to improving skills through hands-on
+          learning and development work. Proficient in mobile and desktop
+          development environments. Using <b>HTML5, JavaScript, ReactJs </b> and
+          other programming languages to produce clean code. Well-organized and
+          collaborative team player with strong communication and analytical
+          abilities.
+        </p>
+        <a href="mailto:farzanullah07@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
