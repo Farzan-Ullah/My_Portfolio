6e8a966aaae649f2197cdbd7996f03eff1f4b4b1
farzan <farzanullah07@gmail.com>
2024-07-11 14:53:16 +0530

    updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,21 +9,22 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Farzan Ullah</h1>
         <p className={styles.description}>
-          {/* I'm a Software Developer with 3 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more! */}
           A Frontend Developer dedicated to improving skills through hands-on
           learning and development work. Using{" "}
           <b>ReactJs, HTML5, JavaScript </b> and other programming languages to
-          produce clean code. Well-organized and collaborative team player with
-          strong communication and analytical abilities.
+          produce clean code. 2+ Years of experience. Well-organized and
+          collaborative team player with strong communication and analytical
+          abilities.
         </p>
         <div className={styles.buttonGroup}>
-          <a
-            href="mailto:farzanullah07@gmail.com"
+          {/* <a
+            href="mailto:farzanullah07@gmailcom"
             className={styles.contactBtn}
+            // target="_blank"
+            // rel="noopener noreferrer"
           >
             Contact Me
-          </a>
+          </a> */}
           <a
             href={getImageUrl(
               "resume/Farzan Ullah - Software Developer - Resume_1_1.pdf"
@@ -31,7 +32,7 @@ export const Hero = () => {
             download="Farzan_Ullah_Resume.pdf"
             className={styles.resumeBtn}
           >
-            Download Resume
+            Download CV
           </a>
         </div>
       </div>
