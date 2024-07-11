eae01fbafbe6eb1aac8c18ffb81e5b9147f8e74f
farzan <farzanullah07@gmail.com>
2024-07-11 15:08:05 +0530

    updates

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -25,7 +25,12 @@ export const ProjectCard = ({
         })}
       </ul>
       <div className={styles.links}>
-        <a href={demo} className={styles.link}>
+        <a
+          href={demo}
+          className={styles.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Demo
         </a>
       </div>
